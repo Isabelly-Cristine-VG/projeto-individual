fe714c14fd0f9a8abdb24a1615c7e001e020627a
Isabelly-Cristine-VG <isabelly.godoi@sptech.school>
2025-05-14 13:58:42 -0300

    login e cadastro funcionando!

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,5 +1,5 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -19,6 +19,7 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
+                        res.status(200).json(resultadoAutenticar)
 
                         aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
                             .then((resultadoAquarios) => {
